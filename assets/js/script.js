d405d354587b7602f2b1003957e74e1c0aca2a9f
GuiLacorte11 <gui.lacorte13@gmail.com>
2025-10-17 11:45:54 -0300

    Refactor menu toggle and remove animations
    Removed cascading animation for menu items and simplified menu toggle functionality.

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,21 +5,11 @@ const headerMenu = document.querySelector(".header__menu"),
 
 menuBtn.addEventListener("click", () => {
   headerMenu.classList.toggle("show");
-
-  // Adiciona animação em cascata para os itens do menu
-  headerMenuItems.forEach((item, index) => {
-    item.style.animation = headerMenu.classList.contains("show")
-      ? `fadeIn 0.3s ease forwards ${index / 7 + 0.3}s`
-      : "none";
-  });
 });
 
 headerMenuItems.forEach((item) => {
   item.addEventListener("click", () => {
     headerMenu.classList.remove("show");
-    headerMenuItems.forEach((item) => {
-      item.style.animation = "none";
-    });
   });
 });
 
